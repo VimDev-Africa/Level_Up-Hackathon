26e26a38633fd8306693ad01952e6545c3909631
Xolani-Ndlangisa <xndlangi@student.wethinkcode.co.za>
2021-07-18 17:31:43 +0200

    objective

--- gamifiedapp/databased/models.py
+++ gamifiedapp/databased/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
 class Player(models.Model):
-    
+    name = models.CharField(max_length=30)
     gender = {("F", "female"), ("M", "male")}
     email = models.EmailField()
+    age = models.IntegerField()
+    
     # name = models.CharField(maxLength=30)
     def __str__(self) :
-        return self.email
+        return self.name
+    
+class Objective(models.Model):
+    title = models.CharField(max_length=30)
+    discription = models.TextField()
+
--- gamifiedapp/databased/serialerzers.py
+++ gamifiedapp/databased/serialerzers.py
@@ -1,7 +1,12 @@
-from .models import Player
+from .models import Objective, Player
 from rest_framework import serializers
 
 class PlayerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Player
+        fields = '__all__'
+        
+class ObjectiveSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Objective
         fields = '__all__'
