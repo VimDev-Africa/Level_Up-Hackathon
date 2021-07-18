c5c1c6297efd2732ea23031744bc33d3bd9ab6af
Xolani-Ndlangisa <xndlangi@student.wethinkcode.co.za>
2021-07-18 13:34:38 +0200

    Project Init

--- gamifiedapp/databased/models.py
+++ gamifiedapp/databased/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Player(models.Model):
+    kind = {("F", "female"), ("M", "male")}
+    email = models.EmailField(unique=True)
+    name = models.CharField()
+    def __str__(self) :
+        return self.name
---
+++ gamifiedapp/methods/methods.py
@@ -0,0 +1,7 @@
+from rest_framework.views import APIView
+
+
+class login(APIView):
+    # permission_classes = (IsAuthenticated,)
+    def post(self, req):
+        pass
